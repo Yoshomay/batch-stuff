1e7a1a383fa9a96b6ba5a3c435c4acb4bab29e9d
Yoshomay <isaiah.m.lange@gmail.com>
2024-08-24 18:44:36 -0700

    uploaded

---
+++ dbQuiz.bat
@@ -0,0 +1,105 @@
+@echo off
+setlocal
+:mainMenu
+cls
+
+echo --------------------------------------------------------------------------------------------------
+echo ^| Dragon Ball quiz by Yoshomay. To answer the questions type ^"1^" ^"2^" ^"3^" or ^"4^", then press enter^|
+echo ^| There are 10 questions in total                                                                ^|
+echo --------------------------------------------------------------------------------------------------
+
+pause
+
+
+:startOfTheQuiz
+set /a score="0"
+cls
+
+
+call :question 1, "Who is Vegeta's brother?", "Gure", "Tarble", "Paragus", "Turles", 2
+call :question 2, "Who voiced Goku in the Ocean dub?", "Peter Kelamis", "Sean Schemmel", "Brian Drummond ", "Christopher Sabat", 1
+call :question 3, "When did the first Dragon Ball Fortnite colab release?", "Chapter 3 Season 4", "Chapter 4 Season 1", "Chapter 3 Season 2", "Chapter 3 Season 3", 4
+call :question 4, "What was the theme song used in the orignal English Dragon Ball Z dub?", "Rock the Dragon", "Cha-La Head Cha-La", "Gotta Find That Dragon Ball", "Mystical Adventure", 1
+call :question 5, "How many times *in the main cannon timeline* has Krillin died?", "1", "2", "3", "4", 3
+call :question 6, "What universe is Giren from?", "9", "11", "7", "2", 3
+call :question 7, "Who is the angel of universe 6", "Martinu", "Whis", "Shin", "Vados", 4
+call :question 8, "Who has not died in the *main cannon timeline*? (excluding Super Buu)", "Goku", "Krillin", "Gohan", "Picolo" 3
+call :question 9, "What movie came right after Bio Broley?", "Fusion Reborn", "Broley Second Coming", "Wrath of the Dragon", "Bojack Unbound", 1
+call :question 10, "'Dragon Ball Minus' is from what manga?" "Dr. Slump", "Jaco the Galactic Patrolman", "Dragon Ball Super", "Cowa", 2
+
+
+:finalMenu
+cls
+echo Your score is %score%/10
+echo.
+echo To try again, enter ^"1^"
+echo To see the answers, enter ^"2^"
+echo To go to my personal website, enter ^"3^"
+echo To speak to customer service, enter ^"4^"
+echo To exit enter anything else
+set playAgain=""
+set /p playAgain=""
+
+if "%playAgain%"=="1" goto :startOfTheQuiz
+if "%playAgain%"=="2" goto :answers
+if "%playAgain%"=="3" start "" "https://yoshomay.epizy.com/" && goto :finalMenu
+if "%playAgain%"=="4" start "" "https://cdn.discordapp.com/attachments/675876648784101386/1276851131946897448/how_may_i_take_your_order.png?ex=66cb07c8&is=66c9b648&hm=dfabb3e5aea3762d31f85545cca644d0dd43ef108e80d6046dc0c00ac0e89a11&/" && goto :finalMenu
+
+exit
+
+:answers
+cls
+echo 1. Who is Vegeta's brother?: Tarble
+echo 2. Who voiced Goku in the Ocean dub?: Peter Kelamis
+echo 3. When did the first Dragon Ball Fortnite colab release?: Chapter 3 season 3
+echo 4. What was the theme song used in the orignal English Dragon Ball Z dub?: Rock the Dragon
+echo 5. How many times *in the main cannon timeline* has Krillin died?: 3
+echo 6. What universe is Giren from?: 7
+echo 7. Who is the angel of universe 6?: Vados
+echo 8. Who has not died in the *main cannon timeline*? (excluding Super Buu): Gohan
+echo 9. What movie came right after Bio Broley?: Fusion Reborn
+echo 10. 'Dragon Ball Minus' is from what manga?: Jaco the Galactic Patrolman
+echo.
+
+pause
+goto finalMenu
+
+
+
+:question
+
+cls
+echo Question %~1:
+echo %~2
+echo.
+echo 1: %~3
+echo 2: %~4
+echo 3: %~5
+echo 4: %~6
+::echo.
+set /p userInput=""
+
+if not "%userInput%"=="1" if not "%userInput%"=="2" if not "%userInput%"=="3" if not "%userInput%"=="4" (
+    cls
+    echo "Please enter 1, 2, 3, or 4"
+    pause
+    echo.
+    goto :question
+)
+
+set /a userInputInt=%userInput%
+if "%userInput%"=="%~7" (
+    set /a score+=1
+    echo.
+    echo Correct!
+    timeout /t 1 /nobreak >nul
+    exit /b 0
+)
+
+echo.
+echo Incorrect!
+timeout /t 2 /nobreak >nul
+exit /b 0
+
+echo how the hell u reading this
+pause
---
+++ menu.bat
@@ -0,0 +1,171 @@
+@echo off
+cls
+:menu
+
+set menuSelection=
+
+Echo What would you like to do?
+Echo 1. View the current time
+Echo 2. See all the files in a certain folder
+Echo 3. Generate a random number
+Echo 4. Exit
+
+set /p menuSelection=""
+
+if %menuSelection%==1 (goto time)
+if %menuSelection%==2 (goto dir)
+if %menuSelection%==3 (goto random)
+if %menuSelection%==4 (exit)
+
+    cls
+    echo "Please enter a valid number"
+    echo.
+    goto :menu
+
+:: -------------------------------------------------------
+
+:time
+cls
+if "%timeFormat%"=="12" (goto time12)
+if "%timeFormat%"=="24" (goto time24)
+
+    :asksForTimeFormat
+    echo "Would you like the time in 12 hour or 24 hour?"
+    set /p timeFormat=
+    if "%timeFormat%"=="12" (goto time12)
+    if "%timeFormat%"=="24" (goto time24)
+
+    set timeFormat=
+    cls
+    echo "Please enter either ^"12^" or ^"24^""
+    echo.
+    goto :asksForTimeFormat
+
+:time12
+set "currentTime=%time%"
+set "hour=%time:~0,2%"
+set "currentTimeMinusHour=%currentTime:~2,-6%" 
+
+if %hour% LSS 12 goto time12am
+
+    set /a hourInt=%hour%
+    set /a hourIntPM=%hourInt% - 12
+    
+    set "currentTimeMinusHour=%currentTime:~2,-6%" 
+    cls
+
+
+    echo The current time is %hourIntPM%%currentTimeMinusHour%PM
+    pause
+
+    cls
+    goto menu
+
+
+:time12am
+
+cls
+if "%hour%"==" 0" set "hour=12"
+echo The current time is %hour%%currentTimeMinusHour%AM
+pause
+
+cls
+goto menu
+
+
+
+
+
+:time24
+set "currentTime=%time%"
+set "currentTime=%currentTime:~0,-6%"
+
+cls
+echo The current time is %currentTime% 
+
+pause
+cls
+
+goto menu
+
+:: -------------------------------------------------------
+
+:dir
+set anotherFolder=""
+cls
+echo What folder do you want to see?
+set /p directory=""
+
+cls
+dir /b %directory%
+echo.
+echo Do you want to see another folder? (y/N)
+set /p anotherFolder=""
+
+cls
+if "%anotherFolder%"=="Y" goto dir
+if "%anotherFolder%"=="y" goto dir
+goto menu
+
+:: -------------------------------------------------------
+
+:random
+setlocal enabledelayedexpansion 
+cls
+
+echo What should the minimum be (default:1)?
+:min
+
+set /p min=""
+
+if not defined min (
+    set /a "minInt=1"
+    goto skiptomax
+)
+
+set /a minInt=%min% 2>nul
+if errorlevel 1 (
+    echo Please enter a valid number ^(0-32767^)
+    ver >nul 
+    
+    set min=""
+    set minInt=""
+    goto min
+)
+
+:skiptomax
+echo.
+echo What should the maximum be? (default: 100)
+:max
+set /p max=""
+
+if not defined max (
+    set /a "maxInt=100"
+    goto randomFinallyDone
+)
+
+set /a maxInt=%max% 2>nul
+if errorlevel 1 (
+    echo Please enter a valid number ^(0-32767^)
+    ver >nul
+    set max=""
+    set maxInt=""
+    goto max
+)
+
+:randomFinallyDone
+set /a result=!random! %% (maxInt - minInt + 1) + minInt
+
+cls
+echo %result%
+echo.
+
+rem echo %minInt% %maxInt%
+echo Would you like another random number? (y/N)
+set /p anotherRandom=""
+
+cls
+if "%anotherRandom%"=="Y" goto randomFinallyDone
+if "%anotherRandom%"=="y" goto randomFinallyDone
+endlocal
+goto menu
